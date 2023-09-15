411fda54a59dfb4b8da79fcb3681251a6fdad0fa
lmcunneen <liam.cunneen282@gmail.com>
2023-09-15 14:12:08 +1000

    Scrolling Text Additions and Song Additions

--- 14 09 Game Jam/Assets/Scripts/ScrollingText.cs
+++ 14 09 Game Jam/Assets/Scripts/ScrollingText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,11 +6,14 @@ using UnityEngine.UI;
 
 public class ScrollingText : MonoBehaviour
 {
-    private string message1 = "My love,\nShould this message reach you, I am near. Engage ship’s Generative Pairing System.\nI miss you.\n- H ";
-    public string message2;
-    public string message3;
-    public string message4;
+    private string message1 = "[[ [ 1 ] NEW MESSAGE(S) ]]";
+    private string message2 = "My love,\n          \nShould this message reach you, I am near.\nEngage ship’s Generative Pairing System.\n          \nI miss you.          - H ";
+    private string message3;
+    private string message4;
     private char[] textArray;
+
+    private Color playerColour;
+    private Color partnerColour;
 
     public Text textComponent;
 
@@ -17,6 +21,21 @@ public class ScrollingText : MonoBehaviour
 
     private void Start()
     {
+        playerColour = Color.white;
+
+        partnerColour.r = 254;
+        partnerColour.g = 108;
+        partnerColour.b = 144;
+
+        AssignMessage();
+    }
+
+    public void ChangeMessage()
+    {
+        Array.Clear(textArray, 0, textArray.Length - 1);
+        textComponent.text = null;
+        increment++;
+
         AssignMessage();
     }
 
@@ -25,21 +44,25 @@ public class ScrollingText : MonoBehaviour
         if (increment == 1)
         {
             textArray = message1.ToCharArray();
+            textComponent.color = playerColour;
         }
 
         else if (increment == 2)
         {
             textArray = message2.ToCharArray();
+            textComponent.color = playerColour;
         }
 
         else if (increment == 3)
         {
             textArray = message3.ToCharArray();
+            textComponent.color = playerColour;
         }
 
         else if (increment == 4)
         {
             textArray = message4.ToCharArray();
+            textComponent.color = playerColour;
         }
 
         else
